47a50d0a6f287a1b8959a937785fa9e0943ccb6d
lewiscutey <949124363@qq.com>
2019-11-20 14:53:31 +0800

    feat(add webpack-exercise): :art:

--- webpack-exercise/src/index.js
+++ webpack-exercise/src/index.js
@@ -1,10 +1,17 @@
-const EventEmitter = require('events');
-const myEmitter = new EventEmitter();
-//on的第一个参数是事件名，之后emit可以通过这个事件名，从而触发这个方法。
-//on的第二个参数是回掉函数，也就是此事件的执行方法
-myEmitter.on('newListener', (param1,param2,param3) => {
-	console.log("newListener111",param1,param2,param3)
-});
-//emit的第一个参数是触发的事件名
-//emit的第二个以后的参数是回调函数的参数。
-myEmitter.emit('newListener',111,222);
+import _ from 'lodash';
+import printMe from './print'
+
+function component() {
+  var element = document.createElement('div');
+  var btn = document.createElement('button');
+  element.innerHTML = _.join(['hello', ' webpack'], '');
+
+  btn.innerHTML = 'Click me';
+  btn.onclick = printMe;
+
+  element.appendChild(btn)
+
+  return element;
+}
+
+document.body.appendChild(component())
--- webpack-exercise/webpack.config.js
+++ webpack-exercise/webpack.config.js
@@ -1,23 +1,38 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   entry: {
-    main: './src/index.js'
+    app: './src/index.js',
+    print: './src/print.js'
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
+    filename: '[name].bundle.js',
+    publicPath: '/',
+    path: path.resolve(__dirname, 'dist')
   },
-  target: 'node',
+  devtool: 'inline-source-map',
+  // devServer: {
+  //   contentBase: './dist'
+  // },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
       }
     ]
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'hello webpack'
+    })
+  ]
+  
 }
