84b2940c093786942f1ae0708318dff1e69a5b8d
lewiscutey <949124363@qq.com>
2019-11-30 22:28:30 +0800

    chore(add images): :bento:

--- README.md
+++ README.md
@@ -3,8 +3,9 @@
 1. **webpack-4-tutorial** 基于Webpack4的官方配置demo
 2. **webpack-demo** 基于webpack4的自定义配置完整demo
 3. **webpack-exercise** 基于webpack4的自起服务和dev-server练习demo
-4. **webpack-plugin-demo** 基于webpack的loader练习demo
-5. **webpack-numbers** 基于webpack4封装的一个`数字转化`的js库
+4. **webpack-images** 基于webpack4的流程图
+5. **webpack-plugin-demo** 基于webpack4的loader练习demo
+6. **webpack-numbers** 基于webpack4封装的一个`数字转化`的js库
 
 > 新手可参考下面这篇文章来一次全流程练习。
 
--- webpack-exercise/webpack.common.js
+++ webpack-exercise/webpack.common.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin')
 
 module.exports = {
   entry: {
@@ -9,6 +11,14 @@ module.exports = {
   output: {
     filename: '[name].[hash].js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['css-loader', 'style-loader']
+      },
+    ]
   },
   plugins: [
     new CleanWebpackPlugin(),
