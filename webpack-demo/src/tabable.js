2aea569a232b65bfa19d465b047ebdc528f4b61b
lewiscutey <949124363@qq.com>
2019-12-03 12:03:47 +0800

    chore(update images): :lipstick:

---
+++ webpack-demo/src/tabable.js
@@ -0,0 +1,25 @@
+const { SyncHook } = require('tapable')
+let h1 = new SyncHook(['options']);
+h1.tap('A', function (arg) {
+  console.log('A', arg);
+  return 'b';
+})
+h1.tap('B', function () {
+  console.log('b')
+})
+h1.intercept({
+  call: (...args) => {
+    console.log(...args, '-----intercept call');
+  },
+  register: (tap) => {
+    console.log(tap, '-----intercept register');
+    return tap;
+  },
+  loop: (...args) => {
+    console.log(...args, '------intercept loop')
+  },
+  tap: (tap) => {
+    console.log(tap, '------intercept tap')
+  }
+})
+h1.call(666);
