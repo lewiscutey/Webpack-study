c21fb33d4b651921572142d97218789e8f94706a
lewiscutey <949124363@qq.com>
2020-01-02 16:47:40 +0800

    feat(add webpack-vue-demo): :bento:

--- README.md
+++ README.md
@@ -4,8 +4,10 @@
 2. **webpack-demo** 基于webpack4的自定义配置完整demo
 3. **webpack-exercise** 基于webpack4的自起服务和dev-server练习demo
 4. **webpack-images** 基于webpack4的流程图
-5. **webpack-plugin-demo** 基于webpack4的loader练习demo
+5. **webpack-loader-demo** 基于webpack4的loader练习demo
 6. **webpack-numbers** 基于webpack4封装的一个`数字转化`的js库
+7. **webpack-plugin-demo** 基于webpack4的plugin练习demo
+7. **webpack-vue-demo** 基于webpack4的vue练习demo
 
 > 新手可参考下面这篇文章来一次全流程练习。
 
---
+++ webpack-vue-demo/vue.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+
+module.exports = {
+  configureWebpack: {
+    entry: {
+      index: './src/index.js',
+      main: './src/main.js'
+    },
+    optimization: {
+      splitChunks: {
+        minSize: 1,
+        maxSize: 0,
+        cacheGroups: {
+          default: false,
+          vendors: {
+            test: /[\\/]node_modules[\\/]/,
+            chunks: 'all',
+            minChunks: 2,
+            name(module) {
+              // 处理node_modules的chunk位置为node_modules下的路径,通过name来控制,如node_modules/vue/dist/vue.runtime.esm;
+              const index = module.resource.indexOf('node_modules')
+              const chunkPath = module.resource
+                .slice(index)
+                .replace(/(.+)\.\w\??(.+)?/, ($0, $1) => $1)
+              // 兼容windows上的路径
+              return chunkPath.split(path.sep).join('/')
+            },
+            priority: 2,
+            reuseExistingChunk: true,
+            enforce: true
+          },
+          chunks: {
+            chunks: 'all',
+            minChunks: 2,
+            name(module) {
+              // 处理正常chunk的位置为相对src下的路径，通过name来控制,如Common/a;
+              const chunkPath = module.resource ? module.resource
+                .replace(/(.+)\.\w\??(.+)?/, ($0, $1) => $1) : 'aaaaa'
+              // 兼容windows上的路径
+              return chunkPath.split(path.sep).join('/')
+            },
+            priority: 1,
+            reuseExistingChunk: true,
+            enforce: true
+          }
+        }
+      }
+    }
+  }
+}
