2b1d88412d172b06b587c041f56abb2335d45183
lewiscutey <949124363@qq.com>
2020-07-23 17:10:31 +0800

    feat(add CopyPluginPlugin): :sparkles:  Deploying stuff

--- webpack-plugin-demo/webpack.config.js
+++ webpack-plugin-demo/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const FileListPlugin = require('./src/plugins/FileListPlugin')
 const ExtractChunksPlugin = require('./src/plugins/ExtractChunksPlugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: "development",
@@ -84,7 +85,12 @@ module.exports = {
       inject: 'head',
       chunks: ['index']
     }),
-    new FileListPlugin()
+    new FileListPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, 'src/copy.txt') },
+      ],
+    }),
     // new ExtractChunksPlugin({
     //   chunkPath: ''
     // })
